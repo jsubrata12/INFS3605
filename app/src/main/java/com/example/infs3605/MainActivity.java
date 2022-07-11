835f325fcab41a101ea01252e66a264aa7d53dd3
Jadon Subrata <jsubrata12@gmail.com>
2022-07-12 00:02:25 +1000

    commit 2

--- app/src/main/java/com/example/infs3605/MainActivity.java
+++ app/src/main/java/com/example/infs3605/MainActivity.java
@@ -6,16 +6,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
+    private LinearLayout button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.button);
+        button = findViewById(R.id.layoutTest);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
