3c85089d7679101832e7d55a5920d278754678f8
Jadon Subrata <jsubrata12@gmail.com>
2022-07-20 21:03:57 +1000

    Added functionality files

--- app/src/main/java/com/example/infs3605/MainActivity.java
+++ app/src/main/java/com/example/infs3605/MainActivity.java
@@ -2,37 +2,60 @@ package com.example.infs3605;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.Fragment.*;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+
+import com.example.infs3605.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
     private LinearLayout button, button1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
 
-        button = findViewById(R.id.layoutTest);
-        button1 = findViewById(R.id.layoutTest2);
 
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, ProjectActivity.class);
-                startActivity(intent);
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.menuHome:
+                    replaceFragment(new HomeFragment());
+                    break;
+                case R.id.menuAccount:
+                    replaceFragment(new AccountFragment());
+                    break;
+                case R.id.menuFavorite:
+                    replaceFragment(new FavoriteFragment());
+                    break;
+                case R.id.menuSettings:
+                    replaceFragment(new SettingsFragment());
+                    break;
             }
+            return true;
         });
 
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this, SDGActivity.class);
-                startActivity(intent);
-            }
-        });
+
+    }
+
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
